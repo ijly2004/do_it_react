866ed33b7d06a34fee35e1349ba43142bc4dacf6
Joseph.Choi <ijly2004@gmail.com>
2020-12-05 11:57:43 +0900

    test

--- do-it-example/src/stories/InputStory.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { storiesOf, storiesof } from '@storybook/react';
-import Input from '../Components/Test/03/Input';
-
-storiesOf('Input', module).add('기본 설정', () => <Input />);
